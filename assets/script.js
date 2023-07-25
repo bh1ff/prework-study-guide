6021b14aa466dd3daba89161e0ee4a4832351704
bh1ff <ahmedayad1111@gmail.com>
2023-07-25 11:26:09 +0100

    new js

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+var shape = "circle";
+var topics = ['HTML','CSS','Git','JavaScript']
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+
+
+
+
+/*
+
+
+
+if ( topic === 'HTML' ) {
+    console.log("HTML On")
+}    
+else if (topic === 'CSS'){
+    console.log("CSS On")
+}
+else if (topic === 'Git'){
+    console.log("Git")
+}
+else if (topic ==='JavaScript'){
+    console.log("JavaScript")
+}
+else {
+    console.log("Please try again.")
+}
+*/
+
